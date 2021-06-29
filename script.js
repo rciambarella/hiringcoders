a9c0c13632bf3dd7cfba866e736fff0730a67187
rbciambarella <rbciambarella@gmail.com>
2021-06-29 19:26:47 -0300

    Novos Aprndizados HTML5 29062021

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-function nome(parametro) {
-    return ""+parametro;
-}
-
-console.log(nome("Aplicação Hiring Code 28062021 -*- 01234567890123456789 -*- Github"));
+alert(" ***** Testando Novamente Amarelo ***** ");
